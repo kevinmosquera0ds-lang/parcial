02c850ce0bda2783c2227ed13fde3bd57382e3b1
Kevin Mosquera <kevin.mosquera.0ds@utap.edu.co>
2026-09-14 19:36:04 -0500

    first commit :

---
+++ examen.js
@@ -0,0 +1,10 @@
+//DATOS PERSONALES 
+const nombre = "Juan";
+const apellido = "Pérez";
+const edad = 30;
+const correo = "kevin.mosquera.0ds@utap.edu.co"
+
+//DATOS DE ACADEMICOS
+const universidad = "UTAP";
+const carrera = "Ingeniería de Sistemas";
+const semestre = 2;
